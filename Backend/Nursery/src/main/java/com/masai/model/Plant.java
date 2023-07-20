c5b58ab7d4946401021e2bea38eff61ac29265c2
Shweta Mahantesh Guranagoudar <115460874+1996shwetamg@users.noreply.github.com>
2023-07-20 21:17:23 +0530

    day3

--- Backend/Nursery/src/main/java/com/masai/model/Plant.java
+++ Backend/Nursery/src/main/java/com/masai/model/Plant.java
@@ -5,7 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,17 +22,47 @@ public class Plant {
 @GeneratedValue(strategy=GenerationType.IDENTITY)
 private Integer plantId;
 @NotNull
-@Min(value=1)
+@Min(value=1 ,message="minimum height should be 1")
 private Integer plantheight;
+@NotNull
+@NotBlank
+@NotEmpty
 private String plantSpread;
+@NotNull
+@NotBlank
+@NotEmpty
 private String commonName;
+@NotNull
+@NotBlank
+@NotEmpty
 private String bloomTime;
+@NotNull
+@NotBlank
+@NotEmpty
 private String medicinalOrCulinaryUse;
+@NotNull
+@NotBlank
+@NotEmpty
 private String difficultyLevel;
+@NotNull
+@NotBlank
+@NotEmpty
 private String temperature;
+@NotNull
+@NotBlank
+@NotEmpty
 private String typeOfPlant;
+@NotNull
+@NotBlank
+@NotEmpty
+@Size(min = 10)
 private String plantDescription;
+@NotNull
+@NotBlank
+@NotEmpty
 private Integer plantsStock;
+@NotNull
 private double plantCost;
+
 
 }
