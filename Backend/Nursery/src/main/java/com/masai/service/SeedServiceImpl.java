7d10e763ee5264461036891c9e0223f1fc2e1bf1
Sumit Kumar <121306952+ksumit01@users.noreply.github.com>
2023-07-23 12:08:47 +0530

    Add files via upload
    Seed Service

--- Backend/Nursery/src/main/java/com/masai/service/SeedServiceImpl.java
+++ Backend/Nursery/src/main/java/com/masai/service/SeedServiceImpl.java
@@ -4,101 +4,133 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import com.masai.exception.PlantException;
 import com.masai.exception.SeedException;
-import com.masai.model.Plant;
 import com.masai.model.Seed;
-import com.masai.repository.SeedRepository;
+import com.masai.repository.SeedsRepository;
 
-import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
-public class SeedServiceImpl implements SeedService{
+public class SeedServiceImpl implements SeedService {
 
 	@Autowired
-	private SeedRepository sRepo;
+	private SeedsRepository seedsRepository;
 	
 	@Override
 	public Seed addSeed(Seed seed) throws SeedException {
-		if(seed == null) throw new SeedException("seed is null") ; 
-		Optional<Seed> cus = sRepo.findById(seed.getSeedId()) ;
-    	if(cus.isPresent()) throw new SeedException("Seed already present in database") ;
-    		sRepo.save(seed) ;
-    		log.info("seed added successfully");
-		return seed;
+		
+		
+		Seed savedSeed = seedsRepository.save(seed);
+		
+		System.out.println(savedSeed);
+		
+		if(savedSeed != null) return savedSeed;
+		
+		
+		throw new SeedException("Input Mis-Match");
 	}
 
 	@Override
-	public Seed updateSeed(Integer seedId, Seed updatedSeed) throws SeedException {
-		Seed existSeed = sRepo.findById(seedId)
-        .orElseThrow(() -> new SeedException("Seed not found"));
-
-		existSeed.setCommonName(updatedSeed.getCommonName());
-		existSeed.setBloomTime(updatedSeed.getBloomTime());
-		existSeed.setWatering(updatedSeed.getWatering());
-		existSeed.setTypeOfSeeds(updatedSeed.getTypeOfSeeds());
-		log.info("seed updated successfully");
-        return sRepo.save(existSeed);
+	public Seed updateSeed(Integer id,Seed seed) throws SeedException {
+		
+		
+		Optional<Seed> optSeed = seedsRepository.findById(id);
+		
+		if(optSeed.isPresent()) {
+			
+			Seed currentSeed = optSeed.get();
+			
+			currentSeed.setWatering(seed.getWatering());
+			currentSeed.setTypeOfSeeds(seed.getTypeOfSeeds());
+			currentSeed.setTemperature(seed.getTemperature());
+			currentSeed.setSeedsStock(seed.getSeedsStock());
+			currentSeed.setSeedsPerPacket(seed.getSeedsPerPacket());
+			currentSeed.setSeedsDescription(seed.getSeedsDescription());
+			currentSeed.setSeedsCost(seed.getSeedsCost());
+			currentSeed.setCommonName(seed.getCommonName());
+			currentSeed.setDifficultyLevel(seed.getDifficultyLevel());
+			currentSeed.setBloomTime(seed.getBloomTime());
+			currentSeed.setSeedId(id); // why do we need to pass Id 
+			
+			Seed savedSeed = seedsRepository.save(currentSeed);
+			
+			return savedSeed;
+		}
+		
+		throw new SeedException("Seed not found with id :"+id);
 	}
 
 	@Override
 	public Seed deleteSeed(Integer seedId) throws SeedException {
-		Optional<Seed> opt = sRepo.findById(seedId);
-		if(opt.isPresent()) {
-			sRepo.deleteById(seedId);
-			log.info("seed deleted successfully");
-			return opt.get();
-		}else {
+	
+		Optional<Seed> optSeed = seedsRepository.findById(seedId);
 		
-			throw new SeedException("Opps! Seed not found");
+		if(optSeed.isPresent()) {
+			
+			Seed deletedSeed = optSeed.get();
+			
+			seedsRepository.deleteById(seedId);
+			
+			return deletedSeed;
 		}
+		
+		throw new SeedException("Seed not found with id :"+seedId);
 	}
 
 	@Override
-	public Seed viewSeed(Integer seedId) throws SeedException {
-		log.info("seed viewed successfully");
-		 return sRepo.findById(seedId)
-	                .orElseThrow(() -> new SeedException("Seed not found"));
+	public Seed viewSeed(int seedId) throws SeedException {
+	
+		Optional<Seed> optSeed = seedsRepository.findById(seedId);
+		
+		if(optSeed.isPresent()) {
+			
+			Seed savedSeed = optSeed.get();
+			
+			return savedSeed;
+		}
+		
+		throw new SeedException("Seed not found with id :"+seedId);
+		
 	}
 
 	@Override
-	public Seed viewSeedByCommonName(String commonName) throws SeedException {
-		Optional<Seed> s=sRepo.findByCommonName(commonName);
-		if(s.isEmpty()) throw  new SeedException("Seed not found");
-		Seed seed=s.get();
-		log.info("seed viewed successfully");
-		return seed;
+	public List<Seed> viewSeed(String commonName) throws SeedException {
+
+		List<Seed> seedList = seedsRepository.getByCommonName(commonName);
+		
+		if(seedList.size() > 0) {
+			
+			return seedList;
+		}
+		
+		throw new SeedException("No Seed found by this common name :"+ commonName);
 	}
 
 	@Override
-	public List<Seed> viewAllseed() throws SeedException {
-		Pageable p = PageRequest.of(0, 5, Sort.by("commonName").ascending()) ;
-		List<Seed> seedList = sRepo.findAll(p).getContent() ;
-    	if(seedList.isEmpty()) throw new SeedException("Empty Seed list") ;
-    	log.info("seed viewed successfully");
-    	return seedList ;
+	public List<Seed> viewAllSeeds() throws SeedException {
+		
+		List<Seed> seedList = seedsRepository.findAll();
+		
+		if(seedList.size() > 0) {
+			
+			return seedList;
+		}
+		
+		throw new SeedException("No Seed found ");
 	}
 
 	@Override
-	public List<Seed> viewAllSeeds(String typeOfSeed) {
-        
-        
-        Pageable p= PageRequest.of(0, 5,Sort.by("typeOfSeed").ascending());
+	public List<Seed> viewAllSeeds(String typeOfSeed) throws SeedException {
+
+		List<Seed> seedList = seedsRepository.getByTypeOfSeeds(typeOfSeed);
 		
-		Page<Seed> seeds=sRepo.findAll(p);
+		if(seedList.size() > 0) {
+			
+			return seedList;
+		}
 		
-		if(seeds.isEmpty())throw new PlantException("No seed Found !!!");
-		
-		log.info("Seeds viewed Successfully");
-		
-		return seeds.toList();
+		throw new SeedException("No Seed found by this type of seed :"+ typeOfSeed);
 	}
 
 }
