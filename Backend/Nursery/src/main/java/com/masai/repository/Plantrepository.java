6ebe85f557c644d33a2dfd0bc0d936ba15112cb1
Shweta Mahantesh Guranagoudar <115460874+1996shwetamg@users.noreply.github.com>
2023-07-23 12:17:06 +0530

    updated

--- Backend/Nursery/src/main/java/com/masai/repository/Plantrepository.java
+++ Backend/Nursery/src/main/java/com/masai/repository/Plantrepository.java
@@ -1,15 +1,19 @@
 package com.masai.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.masai.model.Plant;
 
 public interface Plantrepository extends JpaRepository<Plant,Integer>,PagingAndSortingRepository<Plant,Integer>{
 
-	public Optional<Plant> findByCommonName(String commonname);
+	@Query("select p from Plant p where p.commonName=?1")
+	public List<Plant> getByCommonName(String commonName);
 	
-	public Optional<Plant> findByTypeOfPlant(String typofplant);
+	
+	@Query("select p from Plant p where p.typeOfPlant=?1")
+	public List<Plant> getByTypeOfPlant(String typeOfPlant);
 }
