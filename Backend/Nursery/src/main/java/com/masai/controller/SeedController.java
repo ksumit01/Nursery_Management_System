2bd8c5881b8066abcfbbafa77dc3222f11d0c1a3
Sumit Kumar <121306952+ksumit01@users.noreply.github.com>
2023-07-23 12:12:40 +0530

    Seed Controller

--- Backend/Nursery/src/main/java/com/masai/controller/SeedController.java
+++ Backend/Nursery/src/main/java/com/masai/controller/SeedController.java
@@ -1,5 +1,7 @@
 package com.masai.controller;
 
+
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,62 +16,77 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.masai.model.Plant;
+import com.masai.exception.SeedException;
 import com.masai.model.Seed;
 import com.masai.service.SeedService;
 
 import jakarta.validation.Valid;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class SeedController {
-	@Autowired 
-	  private SeedService sService;
+
+	@Autowired
+	private SeedService seedService;
 	
 	@PostMapping("/seeds")
-	  public ResponseEntity<Seed> addseeds(@Valid @RequestBody Seed seed){
-		  Seed addSeed = sService.addSeed(seed);
-		  return new ResponseEntity<Seed>(addSeed,HttpStatus.ACCEPTED);
-	  }
-	  
-	  @PutMapping("/seeds/{seedId}")
-	  public ResponseEntity<Seed> updateSeed(
-	          @PathVariable("seedId") Integer seedId,
-	           @RequestBody Seed updatedSeed) 
-	  {
-	      Seed updated = sService.updateSeed(seedId, updatedSeed);
-	      return ResponseEntity.ok(updated);
-	  }
-	  
-	  @DeleteMapping("/seed/{seedId}")
-	  public ResponseEntity<Seed> deleteSeed(@PathVariable("seedId") Integer seedId) {
-	      Seed s=sService.deleteSeed(seedId);
-	      return new ResponseEntity<>(s,HttpStatus.OK);
-	  }
-
-	  @GetMapping("/seedId/{seedId}")
-	  public ResponseEntity<Seed> viewSeed(@PathVariable("seedId") Integer seedId) {
-	      Seed seed = sService.viewSeed(seedId);
-	      return ResponseEntity.ok(seed);
-	  }
-	  
-	  @GetMapping("/seeds")
-	  public ResponseEntity<List<Seed>> viewAllSeed() {
-	      List<Seed> seeds = sService.viewAllseed();
-	      return ResponseEntity.ok(seeds);
-	  }
-	  
-	  @GetMapping("/seed/{commonName}")
-	  public ResponseEntity<Seed> viewSeed(@PathVariable("commonName") String commonName) {
-	      Seed seed = sService.viewSeedByCommonName(commonName);
-	      return ResponseEntity.ok(seed);
-	  }
-	  
-	  @GetMapping("/plantViewByTypeOfSeed/{typeOfSeed}")
-		public ResponseEntity<List<Seed>> viewPlantBytypeOfSeedHandller(@PathVariable String typeOfSeed) {
-			
-			List<Seed> seeds = sService.viewAllSeeds(typeOfSeed);
-			
-			return new ResponseEntity<>(seeds, HttpStatus.ACCEPTED);
-		}
+	public ResponseEntity<Seed> addNewSeed(@Valid @RequestBody Seed seed) throws SeedException{
+		
+		Seed seed2 = seedService.addSeed(seed);
+		
+		return new ResponseEntity<Seed>(seed2,HttpStatus.CREATED);
+		
+	}
+	
+	@PutMapping("/seeds/{seedId}")
+	public ResponseEntity<Seed> updateSeed(@PathVariable Integer seedId,@RequestBody Seed seed) throws SeedException{
+		
+		Seed seed2 = seedService.updateSeed(seedId,seed);
+		
+		return new ResponseEntity<Seed>(seed2,HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping("/seeds/{seedId}")
+	public ResponseEntity<Seed> deleteSeed(@PathVariable Integer seedId) throws SeedException{
+		
+		Seed seed2 = seedService.deleteSeed(seedId);
+		
+		return new ResponseEntity<Seed>(seed2,HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/seedsById/{seedId}")
+	public ResponseEntity<Seed> findSeedById(@PathVariable Integer seedId) throws SeedException{
+		
+		Seed seed2 = seedService.viewSeed(seedId);
+		
+		return new ResponseEntity<Seed>(seed2,HttpStatus.FOUND);		
+	}
+	
+	@GetMapping("/seeds")
+	public ResponseEntity<List<Seed>> findAllSeeds() throws SeedException{
+		
+		List<Seed> seedList = seedService.viewAllSeeds();
+		
+		return new ResponseEntity<List<Seed>>(seedList,HttpStatus.FOUND);
+	}
+	
+	@GetMapping("/seedsByCommonName/{commonName}")
+	public ResponseEntity<List<Seed>> findSeedByCommonName(@PathVariable String commonName) throws SeedException{
+		
+		List<Seed> seedList = seedService.viewSeed(commonName);
+		
+		return new ResponseEntity<List<Seed>>(seedList,HttpStatus.FOUND);		
+	}
+	
+	@GetMapping("/seedsByTypeOfSeeds/{typeOfSeeds}")
+	public ResponseEntity<List<Seed>> findSeedByTypeOfSeeds(@PathVariable String typeOfSeeds) throws SeedException{
+		
+		List<Seed> seedList = seedService.viewAllSeeds(typeOfSeeds);
+		
+		return new ResponseEntity<List<Seed>>(seedList,HttpStatus.FOUND);		
+	}
+	
 }
+
