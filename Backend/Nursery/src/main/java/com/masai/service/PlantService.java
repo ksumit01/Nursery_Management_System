5ad66ab3fd87fccc42827767ea53a95cbd39a98f
Sanjay Kumar <115461307+kpsanjay101@users.noreply.github.com>
2023-07-23 13:01:31 +0530

    Add files via upload

--- Backend/Nursery/src/main/java/com/masai/service/PlantService.java
+++ Backend/Nursery/src/main/java/com/masai/service/PlantService.java
@@ -3,14 +3,22 @@ package com.masai.service;
 import java.util.List;
 
 import com.masai.model.Plant;
+import com.masai.exception.PlantException;
 
 public interface PlantService {
-public Plant addPlant(Plant plant);
-public Plant updatePlant(Plant plant);
-public Plant deletePlant(Integer plantid);
-public Plant viewPlant(int plantId);
-public Plant viewplant(String commonName);
-public List<Plant> viewAllPlants();
-public List<Plant> viewAllPlants(String typeOfplant);
 
+
+	public Plant addPlant(Plant plant) throws PlantException;
+
+	public Plant updatePlant(Integer platId,Plant plant) throws PlantException;
+
+	public Plant deletePlant(Integer plantId) throws PlantException;
+
+	public Plant viewPlant(Integer plantId) throws PlantException;
+
+	public List<Plant> viewPlant(String commonName) throws PlantException;
+
+	public List<Plant> viewAllPlants() throws PlantException;
+
+	public List<Plant> viewAllPlants(String typeOfPlant) throws PlantException;
 }
