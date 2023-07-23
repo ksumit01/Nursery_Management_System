e0b5b3240960f5d50e3a7f031efd2788d619a2e8
Sumit Kumar <121306952+ksumit01@users.noreply.github.com>
2023-07-23 12:04:09 +0530

    Seed Modal Class

--- Backend/Nursery/src/main/java/com/masai/model/Seed.java
+++ Backend/Nursery/src/main/java/com/masai/model/Seed.java
@@ -1,15 +1,14 @@
 package com.masai.model;
 
+
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -18,69 +17,61 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-	@Entity
-	@Data
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public class Seed {
-
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Integer seedId;
-		
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		private String commonName;
-		
-		
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		private String bloomTime;
-		
-		
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		private String watering;
-		
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		private String difficultylevel;
-
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		private String temperature;
-		
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		private String typeOfSeeds;
-		
-		@NotNull
-		@NotBlank
-		@NotEmpty
-		@Size(min = 5)
-		private String seedsDescription;
-		
-		@NotNull
-		private Integer seedsStock;
-		
-		@NotNull	
-		private double seedsCost;
-		
-		@NotNull	
-		private Integer seedsPerPacket;
-		
-		@OneToOne(fetch = FetchType.EAGER)
-		private Planter planter;
-		
-		@ManyToOne(cascade = CascadeType.ALL)
-		@JsonIgnore
-		private Orders orders;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Seed {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonIgnore
+	private Integer seedId;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotNull(message = "{null.invalid}")
+	@Size(min = 4,message = "Name cannot be less than 4 characters")
+	private String commonName;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotEmpty(message = "{empty.invalid}")
+	private String bloomTime;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotEmpty(message = "{empty.invalid}")
+	private String watering;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotEmpty(message = "{empty.invalid}")
+	private String difficultyLevel;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotEmpty(message = "{empty.invalid}")
+	private String temperature;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotEmpty(message = "{empty.invalid}")
+	private String typeOfSeeds;
+	
+	@NotBlank(message = "{blank.invalid}")
+	@NotNull(message = "{null.invalid}")
+	@NotEmpty(message = "{empty.invalid}")
+	@Size(min = 10,message = "Description cannot be less than 10 characters")
+	private String seedsDescription;
+	
+	@Min(value = 0, message = "Seed's stock can't be negative")
+	@NotNull(message = "{null.invalid}")
+	private Integer seedsStock;
+	
+	@Min(value = 1, message = "Seed's cost should not be zero")
+	@NotNull(message = "{null.invalid}")
+	private Double seedsCost;
+	
+	@Min(value = 1, message = "There cannot be zero seeds in a packet")
+	@NotNull(message = "{null.invalid}")
+	private Integer seedsPerPacket;
+	
+	
+	
 		
 	}
 
